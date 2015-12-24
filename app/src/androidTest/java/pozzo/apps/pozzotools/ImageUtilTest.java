300a6a27893b6956f613edd1cbb94ef631a53746
Luiz Gustavo Pozzo <lgppozzo@hotmail.com>
2015-12-24 12:31:57 -0200

    Test ImageUtil.

---
+++ app/src/androidTest/java/pozzo/apps/pozzotools/ImageUtilTest.java
@@ -0,0 +1,26 @@
+package pozzo.apps.pozzotools;
+
+import android.graphics.BitmapFactory;
+
+import junit.framework.TestCase;
+
+/**
+ * Some tests on ImageUtil.
+ *
+ * Created by sarge on 24/12/15.
+ */
+public class ImageUtilTest extends TestCase {
+
+	public void testCalculateInSampleSize() throws Exception {
+		BitmapFactory.Options options = new BitmapFactory.Options();
+		options.outHeight = 320;
+		options.outWidth = 320;
+
+		int sampleSize = ImageUtil.calculateInSampleSize(options, 160, 160);
+		assertEquals(2, sampleSize);
+	}
+
+	public void testGetImagePath() throws Exception {
+		//TODO I need to get a sample from some cool devices and also a Samsung
+	}
+}
