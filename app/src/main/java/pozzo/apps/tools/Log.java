bdf025c16aaf92a29534837a943599d73b834e54
Luiz Gustavo Pozzo <lgppozzo@hotmail.com>
2016-02-15 08:15:55 -0200

    Added a log sample and created real possibility for future implementation.

--- app/src/main/java/pozzo/apps/tools/Log.java
+++ app/src/main/java/pozzo/apps/tools/Log.java
@@ -1,12 +1,57 @@
 package pozzo.apps.tools;
 
 /**
- * TODO Log
- * Maybe an auto-created singleton?
- * What about to choose between Mint and Acra?
+ * This class is just an example.
+ * As it does not sound useful without Mint or Acra and I dont wanna import them in here.
+ * TODO check a classforname and decide when to use Mint or Acra
  *
  * @author Luiz Gustavo Pozzo
  * @since 09/02/16
  */
-public class Log {
+class Log {
+	/**
+	 * Just a debug message.
+	 */
+	public static void d(String msg) {
+		if(BuildConfig.DEBUG)
+			System.out.println(msg);
+	}
+
+	/**
+	 * Log an exception to our server.
+	 */
+	public static void e(Exception e) {
+//		Mint.logException(e);
+		if(BuildConfig.DEBUG)
+			e.printStackTrace();
+	}
+
+	/**
+	 * Log an exception with extra fields.
+	 */
+	public static void e(Exception e, String key, String value) {
+//		Mint.logExceptionMessage(key, value, e);
+		if(BuildConfig.DEBUG) {
+			System.out.println(key + ": " + value);
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Anything you to log to our server.
+	 */
+	public static void event(String event) {
+//		Mint.logEvent(event);
+		if(BuildConfig.DEBUG)
+			System.out.println(event);
+	}
+
+	/**
+	 * So we can follow the track back to the root.
+	 */
+	public static void breadcrumb(String breadcrumb) {
+//		Mint.leaveBreadcrumb(breadcrumb);
+		if(BuildConfig.DEBUG)
+			System.out.println("breadcrumb: " + breadcrumb);
+	}
 }
